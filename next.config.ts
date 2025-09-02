808a63ac22e7f68bf5fe4337134ff9cfb910f69d
atChaolong <3130474868@qq.com>
2025-09-02 19:52:19 +0800

    gafhfgdgafdga

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/app/api/convert": [
-      "./public/videos/sample.mp4",
       "./node_modules/ffmpeg-static/ffmpeg",
     ],
     "/app/api/video/watermark": [
-      "./public/videos/sample.mp4",
-      "./public/images/watermark/*.png",
       "./node_modules/ffmpeg-static/ffmpeg",
     ],
   },
